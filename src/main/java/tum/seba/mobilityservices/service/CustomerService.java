a5bc781ec857fda90dd18a0765004a1fa7ab68b4
Nektarios Machner <nektarios.machner@tum.de>
2021-01-24 21:49:33 +0100

    Exercise-Template pushed by Artemis

---
+++ src/main/java/tum/seba/mobilityservices/service/CustomerService.java
@@ -0,0 +1,40 @@
+package tum.seba.mobilityservices.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tum.seba.mobilityservices.entity.Customer;
+import tum.seba.mobilityservices.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	public Customer save(Customer newCustomer) {
+		return customerRepository.save(newCustomer);
+	}
+
+	public Iterable<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	public Customer findById(int customerId) {
+		return customerRepository.findById(customerId).orElse(null);
+	}
+
+	public void deleteById(int customerId) {
+		try {
+			customerRepository.deleteById(customerId);
+		}
+		catch (Exception e) {
+			System.err.println("Unable to delete Customer with ID: " + customerId);
+		}
+	}
+
+	public Iterable<Customer> findCustomerWithUnpaidInvoices() {
+		return customerRepository.findCustomersWithUnpaidInvoices();
+	}
+
+}
